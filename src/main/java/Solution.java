89043a4bb88b633a02b0d71a89a8ee76b68945af
Márta Nagy <kyoukaa98@gmail.com>
2022-03-05 15:45:16 +0100

    Implemented solutions

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,24 @@
-public class Solution {
+import countries.Country;
 
+import java.time.ZoneId;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Solution implements Homework {
+    @Override
+    public String getTheFirstNonIndependentCountryName() {
+        return countries.stream()
+                .filter(Country::isIndependent)
+                .map(Country::getName)
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public List<String> getCountryNamesInUtcPlusEightTimezone() {
+        return countries.stream()
+                .filter(country -> country.getTimezones().contains(ZoneId.of("UTC+08:00")))
+                .map(Country::getName)
+                .toList();
+    }
 }
